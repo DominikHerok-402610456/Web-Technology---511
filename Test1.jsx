87818569e140eb787d6c14728cb86c87ab542509
DominikHerok-402610456 <402610456@my.richfield.ac.za>
2026-09-10 09:54:31 +0200

    Git Repo established and test started.

---
+++ Test1.jsx
@@ -0,0 +1,44 @@
+<DOCTYPE HTML>
+<html>
+
+<head>
+#CSS CIDE GOES HERE. STYLING FOR WEBSITE + LINKS etc
+#Instructions for displaying content
+    <title>
+        Webtech 511 Assignment Test 
+    </title>
+
+    <style>
+
+    </style>
+
+</head>
+
+<body>
+<h1>#Heading Welcome to Webtech 511</h1>
+    
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
